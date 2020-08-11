83a7d982ff7a18d347cb72b00ef4cac6ac46bfb0
poriko2332 <67581656+poriko2332@users.noreply.github.com>
2020-08-11 23:32:06 +0900

    master

--- simulator.js
+++ simulator.js
@@ -4,20 +4,21 @@ window.addEventListener("load", () => {
     document.querySelector("#clear"),
     document.querySelector("#euler"),
     document.querySelector("#runge"),
-    document.querySelector("#result")
+    document.querySelector("#result"),
+    document.querySelectorAll("#scale_aj > button")
   );
 
   sim.draw_axis();
 });
 
 class Simulator_ui {
-  constructor(input, clear, euler, runge, canvas) {
+  constructor(input, clear, euler, runge, canvas, scale) {
     this.input = input;
     this.clear = clear;
     this.euler = euler;
     this.runge = runge;
     this.canvas = canvas;
-    this.scale = 10.0;   //グラフの拡大率,500px基準
+    this.scale = 25.0;   //グラフの拡大率,500px基準
     
     this.clear.addEventListener("click", () => {
       console.log("clear");
@@ -26,24 +27,34 @@ class Simulator_ui {
     });
     this.euler.addEventListener("click", () => {
       let euler_result = new Euler(canvas, input, this.scale);
-      euler_result.next();
+      euler_result.animate();
     });
     this.runge.addEventListener("click", () => {
       let runge_result = new Runge(canvas, input, this.scale);
-      runge_result.next();
+      runge_result.animate();
     })
+    for (let btn of scale) {
+      btn.addEventListener("click", () => {
+        if (btn.getAttribute("number")) {
+          this.scale = btn.getAttribute("number");
+          this.clear_canvas();
+          this.draw_axis();
+          console.log(this.scale);
+        }
+      });
+    }
   }
 
   //座標軸と目盛りを描画
   draw_axis() {
     let ctx = this.canvas.getContext("2d");
-    ctx.strokeStyle = ("rgb(0, 0, 0)");
-    ctx.beginPath();
-    ctx.moveTo(0, this.canvas.height);
-    ctx.lineTo(0, 0);
-    ctx.moveTo(0, this.canvas.height);
-    ctx.lineTo(this.canvas.width, this.canvas.height);
-    ctx.stroke();
+    // ctx.strokeStyle = ("rgb(0, 0, 0)");
+    // ctx.beginPath();
+    // ctx.moveTo(0, this.canvas.height);
+    // ctx.lineTo(0, 0);
+    // ctx.moveTo(0, this.canvas.height);
+    // ctx.lineTo(this.canvas.width, this.canvas.height);
+    // ctx.stroke();
 
     //x軸の目盛り
     for (let i = 0; i <= this.canvas.width; i += this.scale) {
@@ -52,6 +63,7 @@ class Simulator_ui {
       ctx.moveTo(i, 0);
       ctx.lineTo(i, this.canvas.height);
       ctx.stroke();
+      console.log("draw");
     }
 
     //y軸の目盛り
@@ -76,9 +88,10 @@ class Euler {
     this.xinit = parseFloat(input[1].value);
     this.yinit = parseFloat(input[2].value);
     this.prot = parseFloat(input[3].value);
-    this.max = 10.0;
+    this.max = 25.0;
     this.scale = scale;
     this.x_prot = document.querySelector("#x_prot");
+    this.y_prot = document.querySelector("#y_prot");
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
   }
@@ -98,12 +111,36 @@ class Euler {
       this.ctx.stroke();
       console.log(this.canvas.height - this.yinit);
       this.x_prot.innerHTML = String(this.xinit);
+      this.y_prot.innerHTML = String(this.yinit);
       
     } 
   }
 
-  func_f(x) {
-    return 2.0 * x;
+  animate() {
+    let intervalID = setInterval(() => {
+      if (this.xinit >= this.max) {
+        clearInterval(intervalID);
+      }
+      this.yinit += this.prot * this.func_f(this.xinit, this.yinit);   //ここの1.0 * this.xinitを入力した式にしたい。
+      this.xinit += this.prot;
+      console.log("x = " + this.xinit);
+      console.log("y = " + this.yinit);
+
+      this.ctx.lineWidth = 2.0;
+      this.ctx.strokeStyle = "rgb(220, 40, 40)";
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.canvas.width / 2 + this.xinit * this.scale, this.canvas.height / 2 - this.yinit * this.scale);
+      this.ctx.lineTo(this.canvas.width / 2 + this.xinit * this.scale + 1, this.canvas.height / 2 - this.yinit * this.scale + 1);
+      this.ctx.stroke();
+      console.log(this.canvas.height - this.yinit);
+      this.x_prot.innerHTML = String(this.xinit);
+      this.y_prot.innerHTML = String(this.yinit);
+
+    }, 1);
+  }
+
+  func_f(x, y) {
+    return 2.0 * x  ;
   }
 }
 
@@ -114,11 +151,13 @@ class Runge {
     this.prot = parseFloat(input[3].value);
     this.max = 10.0;
     this.scale = scale;
+    this.x_prot = document.querySelector("#x_prot");
+    this.y_prot = document.querySelector("#y_prot");
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
   }
 
-  next(scale) {
+  next() {
     var k1, k2, k3, k4;
     while (this.xinit <= this.max) {
       k1 = this.func_f(this.xinit, this.yinit);
@@ -138,6 +177,32 @@ class Runge {
     }
   }
 
+  animate() {
+    let intervalID = setInterval(() => {
+      if (this.xinit >= this.max) {
+        clearInterval(intervalID);
+      }
+      var k1, k2, k3, k4;
+      k1 = this.func_f(this.xinit, this.yinit);
+      k2 = this.func_f(this.xinit + this.prot / 2.0, this.yinit + this.prot * k1 * this.prot / 2.0);
+      k3 = this.func_f(this.xinit + this.prot / 2.0, this.yinit + this.prot * k2 * this.prot / 2.0);
+      k4 = this.func_f(this.xinit + this.prot, this.yinit + k3 * this.prot);
+      this.yinit += (this.prot / 6.0) * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
+      this.xinit += this.prot;
+
+      this.ctx.lineWidth = 2.0;
+      this.ctx.strokeStyle = "rgb(0, 70, 200)";
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.canvas.width / 2 + this.xinit * this.scale, this.canvas.height / 2 - this.yinit * this.scale);
+      this.ctx.lineTo(this.canvas.width / 2 + this.xinit * this.scale + 1, this.canvas.height / 2 - this.yinit * this.scale + 1);
+      this.ctx.stroke();
+      console.log(this.canvas.height - this.yinit);
+      console.log("x = " + this.xinit);
+      console.log("y = " + this.yinit);
+      this.x_prot.innerHTML = String(this.xinit);
+      this.y_prot.innerHTML = String(this.yinit);
+    }, 1);
+  }
 
   func_f(x, y) {
     return 2.0 * x;
